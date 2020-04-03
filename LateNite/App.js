cdb4ba6f9c82b05eb28b72938819306ed20a7071
wongkh <wongkh@usc.edu>
2020-04-03 01:24:17 -0700

    fix

--- LateNite/App.js
+++ LateNite/App.js
@@ -2,25 +2,9 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './Home';
+import DetailsScreen from './Detail'
 
-function HomeScreen(props) {
-   const { navigation } = props
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-          title="Press me"
-          onPress={() => navigation.navigate('Details')}/>
-    </View>
-  );
-}
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
@@ -28,7 +12,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen}  options={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+        }}/>
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
